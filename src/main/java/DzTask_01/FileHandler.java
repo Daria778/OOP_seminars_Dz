ab68246d077172f92d11dda76cae6707a5ec0a89
Daria <shkatov.pad@gmail.com>
2023-07-12 16:13:40 +0300

    dgw

--- src/main/java/DzTask_01/FileHandler.java
+++ src/main/java/DzTask_01/FileHandler.java
@@ -2,7 +2,7 @@ package DzTask_01;
 
 import java.io.*;
 
-public class FileHandler implements Serializable {
+public class FileHandler implements Serializable, Workable {
     private FileOutputStream io;
     private ObjectOutputStream oi;
     private FileInputStream ol;
@@ -14,10 +14,12 @@ public class FileHandler implements Serializable {
         this.ol = new FileInputStream("FamilyTree.txt");
         this.lo = new ObjectInputStream(ol);
     }
-    public void fileWD(FamilyTree tree) throws IOException{
+    @Override
+    public void fileWD(FamilyTree tree) throws IOException {
         oi.writeObject(tree);
         oi.close();
     }
+    @Override
     public void fileR() throws IOException, ClassNotFoundException {
         FamilyTree tree1 = (FamilyTree) lo.readObject();
         System.out.println(tree1.getFamily());
--- src/main/java/DzTask_01/Workable.java
+++ src/main/java/DzTask_01/Workable.java
@@ -1,2 +1,8 @@
 package DzTask_01;
 
+import java.io.IOException;
+
+public interface Workable {
+    void fileWD(FamilyTree tree) throws IOException;
+    void fileR() throws IOException, ClassNotFoundException;
+}
